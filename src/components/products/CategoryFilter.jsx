f913da324042d1cc08c01c46597f3ac943ad766b
Noufel17 <jn_naili@esi.dz>
2022-08-05 22:17:10 +0100

    added pagination

---
+++ src/components/products/CategoryFilter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+function CategoryFilter({ setColumnFilterValue, preFilteredRows: { length } }) {
+  const [value, setValue] = useState("");
+  const options = [
+    "all",
+    "men's clothing",
+    "women's clothing",
+    "electronics",
+    "jewelery",
+  ];
+  return (
+    <div className="h-12 w-1/6 rounded-md bg-[#F2F2F2] flex items-center p-4 mb-4 cursor-pointer text-left text-bold ">
+      <select
+        // couldn't display length has to make it happen
+        className="bg-transparent w-full"
+        value={value}
+        defaultValue={"all"}
+        onChange={(event) => {
+          setValue(event.target.value);
+          setColumnFilterValue(event.target.value);
+        }}
+      >
+        {options.map((option, index) => {
+          return <option key={index}>{option}</option>;
+        })}
+      </select>
+    </div>
+  );
+}
+
+export default CategoryFilter;
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { useTable, useSortBy, useGlobalFilter } from "react-table";
+import {
+  useTable,
+  useSortBy,
+  useGlobalFilter,
+  useFilters,
+  usePagination,
+} from "react-table";
 import { useMemo } from "react";
 import "./style.css";
-import Journal from "../../svgs/Journal";
 import ArrowDown from "../../svgs/ArrowDown";
 import ArrowUp from "../../svgs/ArrowUp";
 import GlobalFilter from "./GlobalFilter";
-function Products(props) {
+import CategoryFilter from "./CategoryFilter";
+import Edit from "../../svgs/Edit";
+import ArrowRight from "../../svgs/ArrowRight";
+import ArrowLeft from "../../svgs/ArrowLeft";
+function Products() {
   const [products, setProducts] = useState([]);
   const fetchProductsData = async () => {
     const response = await axios
@@ -19,7 +28,6 @@ function Products(props) {
       setProducts(products);
     }
   };
-  // now real dynamic data
 
   const productsData = useMemo(() => [...products], [products]);
   const productsColumns = useMemo(
@@ -33,6 +41,14 @@ function Products(props) {
                   Header: key,
                   accessor: key,
                   Cell: ({ value }) => <img src={value} alt={key} />,
+                  maxWidth: 30,
+                };
+              }
+              if (key === "category") {
+                return {
+                  Header: key,
+                  accessor: key,
+                  filter: "equals",
                 };
               }
               return { Header: key, accessor: key };
@@ -40,7 +56,6 @@ function Products(props) {
         : [],
     [products]
   );
-
   const tableHooks = (hooks) => {
     hooks.visibleColumns.push((columns) => [
       ...columns,
@@ -49,13 +64,13 @@ function Products(props) {
         Header: "journal",
         Cell: ({ row }) => (
           <button
-            className="journal_btn"
+            className="small_btn"
             onClick={() => {
               alert("editing product with id :", row.values.id);
             }}
           >
             <div>
-              <Journal />
+              <Edit />
             </div>
           </button>
         ),
@@ -63,34 +78,67 @@ function Products(props) {
     ]);
   };
   const table = useTable(
-    { columns: productsColumns, data: productsData },
+    {
+      columns: productsColumns,
+      data: productsData,
+      initialState: { pageSize: 2 },
+    },
     useGlobalFilter,
+    useFilters,
     tableHooks,
-    useSortBy
+    useSortBy,
+    usePagination
   );
 
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    gotoPage,
+    pageCount,
     prepareRow,
     preGlobalFilteredRows,
     setGlobalFilter,
+    preFilteredRows,
     state,
+    setFilter,
   } = table;
+  const { pageIndex } = state;
+
+  const pages = Array.from(new Array(pageCount), (val, index) => 1 + index);
+
+  const [columnFilterValue, setColumnFilterValue] = useState("");
+  // listen to filter value changes outside
+  useEffect(() => {
+    if (columnFilterValue !== "") {
+      setFilter(
+        "category",
+        columnFilterValue === "all" ? undefined : columnFilterValue
+      );
+    }
+  }, [columnFilterValue, setFilter]);
 
   useEffect(() => {
     fetchProductsData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <>
-      <div className="w-full flex justify-start items-center ">
+      <div className="w-full flex flex-row justify-evenly items-center  ">
         <GlobalFilter
           preGlobalFilteredRows={preGlobalFilteredRows}
           globalFilter={state.globalFilter}
           setGlobalFilter={setGlobalFilter}
+        />
+        <CategoryFilter
+          setColumnFilterValue={setColumnFilterValue}
+          preFilteredRows={preFilteredRows}
         />
       </div>
 
@@ -126,7 +174,7 @@ function Products(props) {
             ))}
           </thead>
           <tbody {...getTableBodyProps()}>
-            {rows.map((row, index) => {
+            {page.map((row, index) => {
               prepareRow(row);
               return (
                 <tr key={index} {...row.getRowProps()}>
@@ -140,6 +188,82 @@ function Products(props) {
             })}
           </tbody>
         </table>
+      </div>
+      <div className="pagination_wrapper">
+        <button
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+          data-mdb-ripple="true"
+          data-mdb-ripple-color="light"
+          className="page_btn"
+        >
+          <ArrowLeft />
+        </button>
+        {pageCount < 10 &&
+          pages.map((val, index) => {
+            return (
+              <button
+                key={index}
+                value={val}
+                onClick={() => gotoPage(index)}
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+                className={
+                  index === pageIndex
+                    ? "page_btn bg-[rgba(223,208,242)]"
+                    : "page_btn"
+                }
+              >
+                {val}
+              </button>
+            );
+          })}
+        {pageCount >= 10 && (
+          <>
+            <button
+              onClick={() => gotoPage(0)}
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              className={
+                0 === pageIndex ? "page_btn bg-[rgba(223,208,242)]" : "page_btn"
+              }
+            >
+              {1}
+            </button>
+            <button
+              onClick={() => gotoPage(1)}
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              className={
+                1 === pageIndex ? "page_btn bg-[rgba(223,208,242)]" : "page_btn"
+              }
+            >
+              {2}
+            </button>
+            <button className="page_btn">{"..."}</button>
+            <button
+              onClick={() => gotoPage(pageCount - 1)}
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              className={
+                pageCount - 1 === pageIndex
+                  ? "page_btn bg-[rgba(223,208,242)]"
+                  : "page_btn"
+              }
+            >
+              {pageCount}
+            </button>
+          </>
+        )}
+        <button
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+          data-mdb-ripple="true"
+          data-mdb-ripple-color="light"
+          className="page_btn"
+        >
+          <ArrowRight />
+        </button>
       </div>
     </>
   );
